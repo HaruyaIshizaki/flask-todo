61e66fbc59de09a2a1761a9f25ac61566996e0ad
Ishizaki Haruya <haruya916@icloud.com>
2025-01-16 22:34:03 +0900

    todoデータベース作成

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 # app.py
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 @app.route('/')
 def index():
